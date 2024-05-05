04468f040d31f5b772fcdcf69ae56ae5adf82fab
Oscar Buon Ryd <oscardoras@gmail.com>
2024-05-05 14:16:09 +0200

    Game logic added

--- Level.gd
+++ Level.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
 
-@export var spawn_delay: float = 5.
+@export var max_windows: int = 3
+@export var spawn_delay: float = 2.
+var spawn_time: float = 0.
+var window_resource
 
 func _ready():
-	pass # Replace with function body.
-
+	window_resource = preload("res://window/Window.tscn")
 
 func _process(delta):
-	pass
+	spawn_time += delta
+	
+	if spawn_time >= spawn_delay:
+		spawn_time -= spawn_delay
+		if get_child_count() < max_windows:
+			var window = window_resource.instantiate()
+			add_child(window)
+			window.position = get_window().position + get_window().size / 2
--- window/Window.gd
+++ window/Window.gd
@@ -1,14 +1,21 @@
 extends Window
 
 
+@export var speed: int = 100
 var velocity: Vector2
 var screen_size: Vector2i = Vector2i(1920, 1080)
 
 func _ready():
-	velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1))
+	velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized() * speed
 
 func _process(delta):
 	if position.x <= 0 or position.x + size.x >= screen_size.x:
 		velocity.x *= -1
 	if position.y <= 0 or position.y + size.y >= screen_size.y:
 		velocity.y *= -1
+	
+	position += Vector2i(velocity.x * delta, velocity.y * delta)
+
+func _on_input_event(event):
+	if event is InputEventMouseButton and event.button_index == 1 and event.pressed:
+		queue_free()
